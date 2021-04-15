fb4f87cd167e1d31ad1618d581fdc94f7a6a5f7c
Lukas Rydygel <lukas.rydygel@googlemail.com>
2021-04-15 16:00:28 +0200

    check secret

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
   $json = file_get_contents('php://input');
+  $data = json_encode($data);
   
-  file_put_contents('github', $json);
+  file_put_contents('github', var_export($json, true));
+  file_put_contents('server', var_export($_SERVER, true));
